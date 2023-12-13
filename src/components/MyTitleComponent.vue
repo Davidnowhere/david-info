ef9b576f6bb3a24f7f9b2180cf496842fe28a020
Mạnh Bùi <manhbui3676@gmail.com>
2023-12-13 21:25:33 +0700

    fix: responsive name at phone screen

--- src/components/MyTitleComponent.vue
+++ src/components/MyTitleComponent.vue
@@ -20,7 +20,9 @@ export default defineComponent({
          alt="my-avatar" class="inline-block max-sm:hidden h-40 w-40 rounded-full ring-white dark:ring-slate-800" />
   </div>
   <div class="title pl-4 pt-4 max-sm:pl-0 max-sm:flex-wrap max-sm:pt-10 max-sm:justify-end max-sm:items-end">
-    <h1 class="inline-block font-bold text-3xl">{{ myName }}</h1>
+    <div class="block myName h-9 relative">
+      <h1 class="font-bold text-3xl absolute w-max">{{ myName }}</h1>
+    </div>
     <h6 class="">{{ title }}</h6>
   </div>
 </div>
